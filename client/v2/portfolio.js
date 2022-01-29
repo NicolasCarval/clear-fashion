4d5a328f85354845cf2490dc3d436833d2b84d71
Nicolas Carval <nicolascarval@yahoo.fr>
2022-01-29 13:03:26 +0100

    feat(filter by brand):filter by brand among products on current page, fix bug incorrect show default

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -10,15 +10,19 @@ const selectShow = document.querySelector('#show-select');
 const selectPage = document.querySelector('#page-select');
 const sectionProducts = document.querySelector('#products');
 const spanNbProducts = document.querySelector('#nbProducts');
+const selectBrand = document.querySelector('#brand-select');
 
 /**
  * Set global value
  * @param {Array} result - products to display
  * @param {Object} meta - pagination meta info
  */
-const setCurrentProducts = ({result, meta}) => {
+const setCurrentProducts = ({ result, meta }) => {
     currentProducts = result;
     currentPagination = meta;
+    if (selectBrand.value != "all") {
+        currentProducts = GetProductsByBrand(selectBrand.value);
+    }    
 };
 
 /**
@@ -51,24 +55,33 @@ const fetchProducts = async (page = 1, size = 12) => {
  * @param  {Array} products
  */
 const renderProducts = products => {
-  const fragment = document.createDocumentFragment();
-  const div = document.createElement('div');
-  const template = products
-    .map(product => {
-      return `
+    const fragment = document.createDocumentFragment();
+    if (products.length != 0) {
+        const div = document.createElement('div');
+        const template = products
+            .map(product => {
+                return `
       <div class="product" id=${product.uuid}>
         <span>${product.brand}</span>
         <a href="${product.link}">${product.name}</a>
         <span>${product.price}</span>
       </div>
     `;
-    })
-    .join('');
+            })
+            .join('');
 
-  div.innerHTML = template;
-  fragment.appendChild(div);
-  sectionProducts.innerHTML = '<h2>Products</h2>';
-  sectionProducts.appendChild(fragment);
+        div.innerHTML = template;
+        fragment.appendChild(div);
+        sectionProducts.innerHTML = '<h2>Products</h2>';
+        sectionProducts.appendChild(fragment);
+    } else {
+        const div = document.createElement('div');
+        div.innerHTML = `<p><strong>No current products on this page under these conditions</strong></p>`;
+        fragment.appendChild(div);
+        sectionProducts.innerHTML = '<h2>Products</h2>';
+        sectionProducts.appendChild(fragment);
+    }
+  
 };
 
 /**
@@ -131,9 +144,39 @@ selectPage.addEventListener('change', event => {
         .then(() => render(currentProducts, currentPagination));
 });
 
+/**
+ * Filter products by brand on the current page.
+ * Later we could 
+ * @type {[type]}
+ */
+selectBrand.addEventListener('change', event => {
+    if (event.target.value != "all") {
+        renderProducts(GetProductsByBrand(event.target.value))
+    } else {
+        fetchProducts(currentPagination.currentPage, selectShow.value)
+            .then(setCurrentProducts)
+            .then(() => render(currentProducts, currentPagination));
+    }
+});
+
+
+
 document.addEventListener('DOMContentLoaded', async () => {
-  const products = await fetchProducts();
+    const products = await fetchProducts(currentPagination.currentPage, selectShow.value);
 
   setCurrentProducts(products);
   render(currentProducts, currentPagination);
 });
+
+
+//Declaration of all additional functions
+function GetProductsByBrand(brandName) {
+    let brandProducts = [];
+    for (let i = 0; i < currentProducts.length; i++) {
+        if (currentProducts[i].brand == brandName) {
+            brandProducts.push(currentProducts[i]);            
+        }        
+    }
+    console.log(brandProducts);
+    return brandProducts;
+}
