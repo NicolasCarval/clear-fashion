b14e07cfd9579a5010db73f711cdc2a3f502cf12
Nicolas Carval <nicolascarval@yahoo.fr>
2022-02-06 20:07:41 +0100

    feat(scrap dedicated products): scrap all dedicated products, not only from one page anymore

--- server/sandbox.js
+++ server/sandbox.js
@@ -4,19 +4,19 @@ const montlimartbrand = require('./sites/montlimart');
 const adresseparisbrand = require('./sites/adresseparis');
 
 const montlimartLink = 'https://www.montlimart.com/toute-la-collection.html?limit=all';
-const dedicatedLink = 'https://www.dedicatedbrand.com/en/men/news';
+const dedicatedLink = 'https://www.dedicatedbrand.com/en/men/all-men';
 const adresseparisLink = 'https://adresse.paris/630-toute-la-collection';
 
-async function sandbox(eshop = adresseparisLink) {
+async function sandbox(eshop = dedicatedLink) {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
-      //const products = await dedicatedbrand.scrape(eshop);
+      const products = await dedicatedbrand.scrape(eshop);
       //const products2 = await montlimartbrand.scrape(eshop);
-      const products3 = await adresseparisbrand.scrape(eshop);
-      //console.log(products);
+      //const products3 = await adresseparisbrand.scrape(eshop);
+      console.log(products);
       //console.log(products2);
-      console.log(products3);
+      //console.log(products3);
     console.log('done');
     process.exit(0);
   } catch (e) {
--- server/sites/dedicatedbrand.js
+++ server/sites/dedicatedbrand.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
-const {'v5': uuidv5} = require('uuid');
+const { 'v5': uuidv5 } = require('uuid');
 
 /**
  * Parse webpage restaurant
@@ -8,33 +8,56 @@ const {'v5': uuidv5} = require('uuid');
  * @return {Object} restaurant
  */
 const parse = data => {
-  const $ = cheerio.load(data);
+    const $ = cheerio.load(data);
 
-  return $('.productList-container .productList')
-    .map((i, element) => {
-      const link = `https://www.dedicatedbrand.com${$(element)
-        .find('.productList-link')
-        .attr('href')}`;
-      return {
-        'link':link,
-        'brand': 'dedicated',
-        'price': parseInt(
-          $(element)
-            .find('.productList-price')
-            .text()
-        ),
-        'name': $(element)
-          .find('.productList-title')
-          .text()
-          .trim()
-          .replace(/\s/g, ' '),
-        'photo': $(element)
-          .find('.productList-image img')
-          .attr('data-src'),
-        'id': uuidv5(link, uuidv5.URL)
-      };
-    })
-    .get();
+    return $(".paging-showing")
+        .map((i, element) => {
+            return {
+                'nbCurrent': parseInt(
+                    $(element).find('.js-items-current')
+                        .text()
+                        .trim()
+                        .replace(/\s/g, ' ')
+                ),
+                'nbProduct': parseInt(
+                    $(element).find('.js-allItems-total')
+                        .text()
+                        .trim()
+                        .replace(/\s/g, ' ')
+                )
+            };
+        })
+        .get();
+};
+
+const parse2 = data => {
+    const $ = cheerio.load(data);
+
+    return $('.productList-container .productList')
+        .map((i, element) => {
+            const link = `https://www.dedicatedbrand.com${$(element)
+                .find('.productList-link')
+                .attr('href')}`;
+            return {
+                'link': link,
+                'brand': 'dedicated',
+                'price': parseInt(
+                    $(element)
+                        .find('.productList-price')
+                        .text()
+                ),
+                'name': $(element)
+                    .find('.productList-title')
+                    .text()
+                    .trim()
+                    .replace(/\s/g, ' '),
+                'photo': $(element)
+                    .find('.productList-image img')
+                    .attr('data-src'),
+                'id': uuidv5(link, uuidv5.URL)
+            };
+        })
+        .get();
 };
 
 /**
@@ -43,20 +66,39 @@ const parse = data => {
  * @return {Array|null}
  */
 module.exports.scrape = async url => {
-  try {
-    const response = await fetch(url);
+    try {
+        const response = await fetch(url);
 
-    if (response.ok) {
-      const body = await response.text();
+        if (response.ok) {
+            const body = await response.text();
+            const result = parse(body)
+            const nbPage = Math.ceil(result[0]["nbProduct"] / result[0]["nbCurrent"]);
+            let finalresult = []
+            for (let i = 1; i <= nbPage; i++) {
+                const url2 = 'https://www.dedicatedbrand.com/en/men/all-men?p=' + i.toString();               
+                try {
+                    const response2 = await fetch(url2);
+  
+                    if (response2.ok) {
+                        const body2 = await response2.text();
+                        finalresult = finalresult.concat(parse2(body2));
+                    } else {
+                        console.error(response2);
+                        return null
+                    }
+                } catch (error) {
+                    console.error(error);
+                    return null;
+                }
+            }
+            return finalresult;
+        }
 
-      return parse(body);
+        console.error(response);
+
+        return null;
+    } catch (error) {
+        console.error(error);
+        return null;
     }
-
-    console.error(response);
-
-    return null;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
 };
