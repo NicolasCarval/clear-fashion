d2fdb349517b93ce7a9c0fdc74ff62c13a38d0a2
Nicolas Carval <nicolascarval@yahoo.fr>
2022-02-06 17:55:22 +0100

    feat(scrap ADRESSE Paris products): scrapping all products of ADRESSE Paris and display them in console

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,19 +1,22 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./sites/dedicatedbrand');
 const montlimartbrand = require('./sites/montlimart');
+const adresseparisbrand = require('./sites/adresseparis');
 
 const montlimartLink = 'https://www.montlimart.com/toute-la-collection.html?limit=all';
-const dedicatedLink = 'https://www.dedicatedbrand.com/en/men/news'
+const dedicatedLink = 'https://www.dedicatedbrand.com/en/men/news';
+const adresseparisLink = 'https://adresse.paris/630-toute-la-collection';
 
-async function sandbox(eshop = montlimartLink) {
+async function sandbox(eshop = adresseparisLink) {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} source`);
 
       //const products = await dedicatedbrand.scrape(eshop);
-      const products2 = await montlimartbrand.scrape(eshop);
-
+      //const products2 = await montlimartbrand.scrape(eshop);
+      const products3 = await adresseparisbrand.scrape(eshop);
       //console.log(products);
-      console.log(products2);
+      //console.log(products2);
+      console.log(products3);
     console.log('done');
     process.exit(0);
   } catch (e) {
