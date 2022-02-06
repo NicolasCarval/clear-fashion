67b0cd4ea40b1dc2dbcd71c76552425c69b0968d
Nicolas Carval <nicolascarval@yahoo.fr>
2022-02-06 20:36:08 +0100

    feat(save scrapped products): save all products to a JSON file

--- server/sandbox.js
+++ server/sandbox.js
@@ -3,28 +3,38 @@ const dedicatedbrand = require('./sites/dedicatedbrand');
 const montlimartbrand = require('./sites/montlimart');
 const adresseparisbrand = require('./sites/adresseparis');
 
-const montlimartLink = 'https://www.montlimart.com/toute-la-collection.html?limit=all';
-const dedicatedLink = 'https://www.dedicatedbrand.com/en/men/all-men';
-const adresseparisLink = 'https://adresse.paris/630-toute-la-collection';
+const fs = require('fs');
 
-async function sandbox(eshop = dedicatedLink) {
-  try {
-    console.log(`🕵️‍♀️  browsing ${eshop} source`);
-
-      const products = await dedicatedbrand.scrape(eshop);
-      //const products2 = await montlimartbrand.scrape(eshop);
-      //const products3 = await adresseparisbrand.scrape(eshop);
-      console.log(products);
-      //console.log(products2);
-      //console.log(products3);
-    console.log('done');
-    process.exit(0);
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
+const links = {
+    'montlimartLink': 'https://www.montlimart.com/toute-la-collection.html?limit=all',
+    'dedicatedLink': 'https://www.dedicatedbrand.com/en/men/all-men',
+    'adresseparisLink': 'https://adresse.paris/630-toute-la-collection'
 }
 
-const [,, eshop] = process.argv;
+async function sandbox(eshop = "eshop") {
+    try {
+        console.log(`🕵️‍♀️  browsing ${eshop} source`);
+
+        const products = await dedicatedbrand.scrape(links["dedicatedLink"]);
+        console.log("dedicated is done")
+        const products2 = await montlimartbrand.scrape(links["montlimartLink"]);
+        console.log("montlimart is done")
+        const products3 = await adresseparisbrand.scrape(links["adresseparisLink"]);
+        console.log("adresse paris is done")
+
+        const finalProducts = products.concat(products2, products3);
+        console.log(finalProducts.length);
+
+        fs.writeFileSync("./products.json", JSON.stringify(finalProducts, null, 4));
+
+        console.log('done');
+        process.exit(0);
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+const [, , eshop] = process.argv;
 
 sandbox(eshop);
