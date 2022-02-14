068f86da81ee43b68425ae61adb02b95e7caff28
Nicolas Carval <nicolascarval@yahoo.fr>
2022-02-14 18:15:07 +0100

    feat(mongoDB): retrieve only recent products

--- server/server.js
+++ server/server.js
@@ -36,16 +36,43 @@ const ProductsSortedPrice = async (asc=1) => {
     }   
 }
 
+const ProductsSortedDate = async (asc = 1) => {
+    const products = await collection.find({}).sort({ "date": asc }).toArray();
+    for (let i = 0; i < 10; i++) {
+        console.log(products[i]);
+    }
+}
+
+const ProductsRecent = async () => {
+    const recentDate = new Date()
+    const pastDate = recentDate.getDate() - 14;
+    recentDate.setDate(pastDate);
+
+    const dd = String(recentDate.getDate()).padStart(2, '0');
+    const mm = String(recentDate.getMonth() + 1).padStart(2, '0'); //January is 0!
+    const yyyy = recentDate.getFullYear();
+
+    const recent = yyyy + '-' + mm + '-' + dd;
+    console.log(recent)
+    const products = await collection.find({ "date": { $gte: recent } }).toArray();
+    for (let i = 0; i < 10; i++) {
+        console.log(products[i]);
+    }
+}
+
 
 const connection = async () => {
     const client = await MongoClient.connect(MONGODB_URI, { 'useNewUrlParser': true });
     const db = client.db(MONGODB_DB_NAME)
     collection = db.collection('products');
-    //await findNumber();
+    await findNumber();
     //await Brands();
     //await ProductsBrands();
     //await ProductsPrice(10);
-    await ProductsSortedPrice(-1);
+    //await ProductsSortedPrice(-1);
+    //await ProductsSortedDate();
+    await ProductsRecent();
+    await client.close()
     process.exit(0);
 }
 
